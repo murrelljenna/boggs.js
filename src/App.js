da275ab346524b0e9a63ff528384007e54dd234e
Jenna Murrell <murrelljenna@gmail.com>
2020-12-04 22:24:03 -0500

    Added prettier configuration. Ran prettier on everything.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import logo from './logo.svg';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Contacts from './components/Contacts.js';
-import MainNavbar from './components/MainNavbar.js';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Contacts from "./components/Contacts.js";
+import MainNavbar from "./components/MainNavbar.js";
+import { DataType, EditingMode, SortingMode } from "ka-table/enums";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const ContactsFunc = (props) => {
-    return <Contacts 
-        model={'contacts'}
-        {...props}
+  return (
+    <Contacts
+      {...props}
+      model={"contacts"}
+      columns={[
+        { key: "first_name", title: "First Name", dataType: DataType.String },
+        { key: "last_name", title: "Last Name", dataType: DataType.String },
+        { key: "address", title: "Address", dataType: DataType.String },
+        {
+          key: "unit_number",
+          title: "Unit",
+          dataType: DataType.String,
+          style: { width: 50 },
+        },
+        {
+          key: "email_address",
+          isEditable: true,
+          title: "Email Address",
+          dataType: DataType.String,
+        },
+        { key: "editColumn", style: { width: 75 } },
+      ]}
     />
-}
+  );
+};
+
+const BuildingsFunc = (props) => {
+  return (
+    <Contacts
+      {...props}
+      model={"buildings"}
+      columns={[
+        {
+          key: "street_number",
+          title: "Street Number",
+          dataType: DataType.String,
+        },
+        { key: "street_name", title: "Street Name", dataType: DataType.String },
+        { key: "postal_code", title: "Postal Code", dataType: DataType.String },
+        { key: "editColumn", style: { width: 75 } },
+      ]}
+    />
+  );
+};
 
 function App() {
   return (
@@ -19,7 +59,8 @@ function App() {
         <MainNavbar></MainNavbar>
 
         <Switch>
-          <Route path='/contacts/' component={Contacts} />
+          <Route path="/contacts/" component={ContactsFunc} />
+          <Route path="/buildings/" component={BuildingsFunc} />
         </Switch>
       </main>
     </div>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,304 +3,352 @@
 // vim: set shiftwidth=4
 
 import "ka-table/style.css";
-import React, { Component, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 
-import { ITableProps, kaReducer, Table } from 'ka-table';
-import { DataType, EditingMode, SortingMode } from 'ka-table/enums';
-import { hideLoading, showLoading, closeEditor, updateCellValue, updateEditorValue } from 'ka-table/actionCreators';
-import { DispatchFunc } from 'ka-table/types';
-import { closeRowEditors, openRowEditors, saveRowEditors, showNewRow, saveNewRow, hideNewRow, deleteRow } from 'ka-table/actionCreators';
+import { ITableProps, kaReducer, Table } from "ka-table";
+import { DataType, EditingMode, SortingMode } from "ka-table/enums";
+import {
+  hideLoading,
+  showLoading,
+  closeEditor,
+  updateCellValue,
+  updateEditorValue,
+} from "ka-table/actionCreators";
+import { DispatchFunc } from "ka-table/types";
+import {
+  closeRowEditors,
+  openRowEditors,
+  saveRowEditors,
+  showNewRow,
+  saveNewRow,
+  hideNewRow,
+  deleteRow,
+} from "ka-table/actionCreators";
 
-const AddButton = ({
-  dispatch,
-}) => {
- return (
-  <div className='plus-cell-button'>
-    <img
-      src='https://komarovalexander.github.io/ka-table/static/icons/plus.svg'
-      alt='Add New Row'
-      title='Add New Row'
-      onClick={() => dispatch(showNewRow())}
-    />
-  </div>
- );
-};
-
-const EditButton = ({
-  dispatch, rowKeyValue
-}) => {
+const AddButton = ({ dispatch }) => {
   return (
-   <div className='edit-cell-button'>
-     <img
-      src='https://komarovalexander.github.io/ka-table/static/icons/edit.svg'
-      alt='Edit Row'
-      title='Edit Row'
-      onClick={() => { dispatch(openRowEditors(rowKeyValue))}}
-    />
-   </div>
+    <div className="plus-cell-button">
+      <img
+        src="https://komarovalexander.github.io/ka-table/static/icons/plus.svg"
+        alt="Add New Row"
+        title="Add New Row"
+        onClick={() => dispatch(showNewRow())}
+      />
+    </div>
   );
 };
 
-const SaveNewRowButton = ({
-  dispatch
-}) => {
-  const saveNewData = () => {
-    let rowKeyValue = -1;
-    dispatch(saveNewRow(rowKeyValue, {
-      validate: true
-    }));
-  };
+const EditButton = ({ dispatch, rowKeyValue }) => {
   return (
-   <div className='buttons'>
-    <img
-      src='https://komarovalexander.github.io/ka-table/static/icons/save.svg'
-      className='save-cell-button'
-      alt='Save'
-      title='Save'
-      onClick={saveNewData}
-    />
-    <img
-      src='https://komarovalexander.github.io/ka-table/static/icons/close.svg'
-      className='close-cell-button'
-      alt='Cancel'
-      title='Cancel'
-      onClick={() => dispatch(hideNewRow())}
-    />
-   </div>
- );
+    <div className="edit-cell-button">
+      <img
+        src="https://komarovalexander.github.io/ka-table/static/icons/edit.svg"
+        alt="Edit Row"
+        title="Edit Row"
+        onClick={() => {
+          dispatch(openRowEditors(rowKeyValue));
+        }}
+      />
+    </div>
+  );
 };
 
-const SaveButton = ({
-  dispatch, rowKeyValue, rowData
-}) => {
+const SaveNewRowButton = ({ dispatch }) => {
+  const saveNewData = () => {
+    let rowKeyValue = -1;
+  };
   return (
-    <div className='buttons'
-      style={{display: 'flex', justifyContent: 'space-between'}} >
+    <div className="buttons">
       <img
-        src='https://komarovalexander.github.io/ka-table/static/icons/save.svg'
-        className='save-cell-button'
-        alt='Save'
-        title='Save'
+        tabIndex="0"
+        src="https://komarovalexander.github.io/ka-table/static/icons/save.svg"
+        className="save-cell-button"
+        alt="Save"
+        title="Save"
         onClick={() => {
-          dispatch(saveRowEditors(rowKeyValue, {
-            validate: true,
-            rowKeyValue: rowKeyValue,
-          }));
+          console.log("tiddies");
+          dispatch(
+            saveNewRow(-1, {
+              validate: true,
+            })
+          );
+        }}
+        onKeyDown={(event) => {
+          console.log("Arf");
+          if (event.keycode === 13) {
+            saveNewData();
+          }
         }}
       />
       <img
-        src='https://komarovalexander.github.io/ka-table/static/icons/close.svg'
-        className='close-cell-button'
-        alt='Cancel'
-        title='Cancel'
+        src="https://komarovalexander.github.io/ka-table/static/icons/close.svg"
+        className="close-cell-button"
+        alt="Cancel"
+        title="Cancel"
+        onClick={() => dispatch(hideNewRow())}
+      />
+    </div>
+  );
+};
+
+const SaveButton = ({ dispatch, rowKeyValue, rowData }) => {
+  return (
+    <div
+      className="buttons"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <img
+        tabIndex="0"
+        src="https://komarovalexander.github.io/ka-table/static/icons/save.svg"
+        className="save-cell-button"
+        alt="Save"
+        title="Save"
+        onClick={() => {
+          dispatch(
+            saveRowEditors(rowKeyValue, {
+              validate: true,
+              rowKeyValue: rowKeyValue,
+            })
+          );
+        }}
+        onKeyDown={(event) => {
+          if (event.keyCode === 13) {
+            dispatch(
+              saveRowEditors(rowKeyValue, {
+                validate: true,
+                rowKeyValue: rowKeyValue,
+              })
+            );
+          }
+        }}
+      />
+      <img
+        src="https://komarovalexander.github.io/ka-table/static/icons/close.svg"
+        className="close-cell-button"
+        alt="Cancel"
+        title="Cancel"
         onClick={() => {
           dispatch(closeRowEditors(rowKeyValue));
         }}
       />
-        <img
-          src='https://komarovalexander.github.io/ka-table/static/icons/delete.svg'
-          className='delete-row-column-button'
-          onClick={() => dispatch(deleteRow(rowKeyValue))}
-          alt=''
-        />
-   </div >
- );
+      <img
+        src="https://komarovalexander.github.io/ka-table/static/icons/delete.svg"
+        className="delete-row-column-button"
+        onClick={() => dispatch(deleteRow(rowKeyValue))}
+        alt=""
+      />
+    </div>
+  );
 };
 
-const AddressEditor = ({
-  column, rowKeyValue, dispatch, value,
-}) => {
-    const [buildings, setBuildings] = useState([]);
-    useEffect(() => {
-        axios.get("http://localhost:8000/buildings/").then((res) => {
-            setBuildings(JSON.parse(res.data).map(building => ({
-                address: `${building.fields.street_number} ${building.fields.street_name}`,
-                id: building.pk
-            })));
-        }).catch((err) => {
-            console.log(err)
-        });
-    }, []);
+const AddressEditor = ({ column, rowKeyValue, dispatch, value }) => {
+  const [buildings, setBuildings] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/buildings/")
+      .then((res) => {
+        setBuildings(
+          JSON.parse(res.data).map((building) => ({
+            address: `${building.fields.street_number} ${building.fields.street_name}`,
+            id: building.pk,
+          }))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(value);
+  const [editorValue, setValue] = useState(value);
 
-    const close = () => {
-        dispatch(closeEditor(rowKeyValue, column.key));
-    };
-
-    const [editorValue, setValue] = useState(value);
-
-    return (
-        <div>
-          <select
-            value={editorValue}
-            className='form-control'
-            autoFocus={true}
-            defaultValue={editorValue}
-            onBlur={() => {
-                dispatch(updateEditorValue(rowKeyValue, column.key, editorValue));
-            }}
-            onChange={(event) => {
-                setValue(event.target.value);
-            }}>
-
-            <option key={5} value={'test'}>{'Test'}</option>
-            {buildings.map((building) => (
-                <option key={building.id} value={building.id}>{building.address}</option>
-            ))}
-          </select>
-        </div >
-    );
-}
+  return (
+    <div>
+      <select
+        value={editorValue}
+        className="form-control"
+        onBlur={() => {
+          dispatch(updateEditorValue(rowKeyValue, column.key, editorValue));
+        }}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      >
+        {buildings.map((building) => (
+          <option key={building.id} value={building.id}>
+            {building.address}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default class Contacts extends React.Component {
-    getNewRow = () => {
-        return this.state.props.data;
-    }
+  getNewRow = () => {
+    return this.state.props.data;
+  };
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            props: { 
-                columns: [
-                    { key: 'first_name', title: 'First Name', dataType: DataType.String },
-                    { key: 'last_name', title: 'Last Name', dataType: DataType.String },
-                    { key: 'address', title: 'Address', dataType: DataType.String },
-                    { key: 'unit_number', title: 'Unit', dataType: DataType.String, style: { width: 50 } },
-                    { key: 'email_address', isEditable: true, title: 'Email Address', dataType: DataType.String },
-                    { key: 'editColumn', style: { width: 75 } },
-                ],
-                data: [],
-                rowKeyField:'id',
-                  loading: {
-                    enabled: true,
-                    text: 'Loading Data..'
-                  },
-                sortingMode: SortingMode.Single,
-                getNewRow: this.getNewRow
-            }
-        }
+    this.state = {
+      props: {
+        data: [],
+        columns: this.props.columns,
+        rowKeyField: "id",
+        loading: {
+          enabled: true,
+          text: "Loading Data..",
+        },
+        sortingMode: SortingMode.Single,
+        getNewRow: this.getNewRow,
+      },
+    };
 
-        this.dispatch = this.dispatch.bind(this);
-        this.update = this.update.bind(this);
-    }
+    this.dispatch = this.dispatch.bind(this);
+    this.update = this.update.bind(this);
+  }
 
-    componentDidMount() {
-        this.dispatch(showLoading('Loading Data...'));
-        axios.get(`http://localhost:8000${this.props.match.path}`).then((res) => {
-            this.setState(oldState => ({
-                props: {
-                    ...oldState.props,
-                    data: JSON.parse(res.data).map((contact) => ({ id: contact.pk, ...contact.fields})),
-                }
-
-            }));
-            this.dispatch(hideLoading());
-        }).catch((err) => {
-            console.log(err)
-        });
-    }
-
-    create = (data) => {
-        this.dispatch(showLoading('Creating...'));
-        console.log(data);
-        return axios.post(`http://localhost:8000${this.props.match.path}`, data);
-    }
-
-    update = (pk, data) => {
-        axios.patch(`http://localhost:8000${this.props.match.path}${pk}/`, data).then((res) => {
-            this.dispatch(hideLoading());
-        }).catch((err) => {
-            console.log(err);
-        });
-    }
-    
-    apiDelete = (pk) => {
-        axios.delete(`http://localhost:8000${this.props.match.path}${pk}/`).then(res => {
-            this.dispatch(hideLoading());
-        }).catch((err) => {
-            console.log(err);
-        });
-    }
-
-    dispatch = (action) => {
-        this.setState(oldState => ({
-            props: kaReducer(oldState.props, action)
+  componentDidMount() {
+    this.dispatch(showLoading("Loading Data..."));
+    axios
+      .get(`http://localhost:8000/${this.props.model}/`)
+      .then((res) => {
+        this.setState((oldState) => ({
+          props: {
+            ...oldState.props,
+            data: JSON.parse(res.data).map((object) => ({
+              id: object.pk,
+              ...object.fields,
+            })),
+          },
         }));
+        this.dispatch(hideLoading());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        switch (action.type) {
-            case "SaveRowEditors":
-                this.dispatch(showLoading('Updating...'));
-                let row = this.getEditableRow(action.rowKeyValue).reduce((row, cell) => (row[cell.columnKey] = cell.editorValue, row) ,{});
-                this.update(action.rowKeyValue, row);
-                
-                break;
-            case "SaveNewRow":
-                let data = this.getNewRow().reduce((row, cell) => (row[cell.columnKey] = cell.editorValue, row) ,{});
-                this.create(JSON.stringify(data)).then((res) => {
-                    action.rowKeyValue = res.data.id;
-                    this.dispatch(hideLoading());
-                }).catch((err) => {
-                    console.log(err);
-                });
-                break;
+  create = (data) => {
+    this.dispatch(showLoading("Creating..."));
+    return axios.post(`http://localhost:8000/${this.props.model}/`, data);
+  };
 
-            case "DeleteRow":
-                console.log('What');
-                this.dispatch(showLoading('Deleting Row...'));
-                this.apiDelete(action.rowKeyValue);
-                break;
-        }
-    }
+  update = (pk, data) => {
+    axios
+      .patch(`http://localhost:8000/${this.props.model}/${pk}/`, data)
+      .then((res) => {
+        this.dispatch(hideLoading());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    getNewRow = () => {
-        return this.state.props.editableCells.filter(cell => JSON.stringify(cell.rowKeyValue) == JSON.stringify({}));
-    }
+  apiDelete = (pk) => {
+    axios
+      .delete(`http://localhost:8000/${this.props.model}/${pk}/`)
+      .then((res) => {
+        this.dispatch(hideLoading());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    getEditableRow = (pk) => {
-        return this.state.props.editableCells.filter(cell => cell.rowKeyValue == pk);
-    }
+  dispatch = (action) => {
+    this.setState((oldState) => ({
+      props: kaReducer(oldState.props, action),
+    }));
 
-    getRow = (pk) => {
-        return this.state.props.data.find(row => row.id = pk);
-    }
-
-	render() {
-        return (
-            <Table 
-                {...this.state.props}
-                dispatch={this.dispatch}
-                childComponents={{
-                    cellText: {
-                        content: (props) => {
-                            if (props.column.key === 'editColumn'){
-                                return <EditButton {...props}/>
-                            }
-                        }
-                    },
-                    cellEditor: {
-                        content: (props) => {
-                            if (props.column.key === 'editColumn'){
-                                // If this is a new row (without a rowkeyvalue), lets use a different save button
-                                if (JSON.stringify(props.rowKeyValue) === '{}') {
-                                    return <SaveNewRowButton {...props}/>
-                                } else {
-                                    return <SaveButton {...props}/>
-                                }
-                            } else if (props.column.key === 'address') {
-                                return <AddressEditor {...props}/>
-                            }
-                        }
-                    },
-                    headCell: {
-                        content: (props) => {
-                            if (props.column.key === 'editColumn'){
-                                return <AddButton {...props}/>;
-                            }
-                        }
-                    }
-                }}
-            />
+    switch (action.type) {
+      case "SaveRowEditors":
+        this.dispatch(showLoading("Updating..."));
+        let row = this.getEditableRow(action.rowKeyValue).reduce(
+          (row, cell) => ((row[cell.columnKey] = cell.editorValue), row),
+          {}
         );
-	}
+        this.update(action.rowKeyValue, row);
+
+        break;
+      case "SaveNewRow":
+        let data = this.getNewRow().reduce(
+          (row, cell) => ((row[cell.columnKey] = cell.editorValue), row),
+          {}
+        );
+        this.create(JSON.stringify(data))
+          .then((res) => {
+            action.rowKeyValue = res.data.id;
+            this.dispatch(hideLoading());
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        break;
+
+      case "DeleteRow":
+        this.dispatch(showLoading("Deleting Row..."));
+        this.apiDelete(action.rowKeyValue);
+        break;
+      case "OpenRowEditors":
+        break;
+    }
+  };
+
+  getNewRow = () => {
+    return this.state.props.editableCells.filter(
+      (cell) => JSON.stringify(cell.rowKeyValue) == JSON.stringify({})
+    );
+  };
+
+  getEditableRow = (pk) => {
+    return this.state.props.editableCells.filter(
+      (cell) => cell.rowKeyValue == pk
+    );
+  };
+
+  getRow = (pk) => {
+    return this.state.props.data.find((row) => (row.id = pk));
+  };
+
+  render() {
+    return (
+      <Table
+        {...this.state.props}
+        dispatch={this.dispatch}
+        childComponents={{
+          cellText: {
+            content: (props) => {
+              if (props.column.key === "editColumn") {
+                return <EditButton {...props} />;
+              }
+            },
+          },
+          cellEditor: {
+            content: (props) => {
+              if (props.column.key === "editColumn") {
+                // If this is a new row (without a rowkeyvalue), lets use a different save button
+                if (JSON.stringify(props.rowKeyValue) === "{}") {
+                  return <SaveNewRowButton {...props} />;
+                } else {
+                  return <SaveButton {...props} />;
+                }
+              } else if (props.column.key === "address") {
+                return <AddressEditor {...props} />;
+              }
+            },
+          },
+          headCell: {
+            content: (props) => {
+              if (props.column.key === "editColumn") {
+                return <AddButton {...props} />;
+              }
+            },
+          },
+        }}
+      />
+    );
+  }
 }
